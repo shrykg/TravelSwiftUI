241ce284583e11b1c9b2115ae4b4acbe01646568
Shreyak Godala <shreyakgodala2909@gmail.com>
2021-05-04 13:50:01 +0530

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Shreyak Godala on 04/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
